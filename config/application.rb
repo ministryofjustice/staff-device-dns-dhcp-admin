72580bfa5b9f9576778c35415e848303c5eee5cc
Andy Mitchell <me@themitchell.co.uk>
2020-08-12 17:06:19 +0100

    Add rspec and remove test_unit

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+# see https://guides.rubyonrails.org/initialization.html
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
+# require "rails/test_unit/railtie"
+require "sprockets/railtie"
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,5 +30,10 @@ module StaffDeviceDnsDhcpAdmin
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
