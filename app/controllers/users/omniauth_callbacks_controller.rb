ae14908d4350ee9bf7e604598b975d8eeaae910c
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-08-13 09:22:46 +0100

    Simplify omniauth callback to just use sign_in_and_redirect
    The previous implementation, based on the facebook example in the
    devise docs (https://github.com/heartcombo/devise/wiki/OmniAuth:-Overview)
    redirected the user to a signup page if the User record did not already exist
    in the database.
    
    We simply want to log the user in regardless of if it is a new User record
    or not as there is no registration step

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def cognito_idp
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Cognito IDP") if is_navigational_format?
-    end
+    sign_in_and_redirect @user
   end
 
   def failure
