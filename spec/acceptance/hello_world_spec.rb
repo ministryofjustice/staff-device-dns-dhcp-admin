d880587dacad4e329c0a15ae90f3660482e727da
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-08-13 14:15:00 +0100

    Update hello world spec to display the login page if the user is not signed in
    Co-authored-by: Cait <caitlin@madetech.com>
    Co-authored-by: Efua Akumanyi <efua.akumanyi@gmail.com>

--- spec/acceptance/hello_world_spec.rb
+++ spec/acceptance/hello_world_spec.rb
@@ -1,8 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "GET /", type: :feature do
-  it "displays hello" do
+
+  it "displays hello when signed in" do
+    pending "Log in functionality not yet implemented"
+
+    visit "/sign_in"
+    click_link("Sign in with Cognito")
     visit "/"
     expect(page).to have_content "Hello from Staff Device"
+  end
+
+  it "displays log in when not signed in" do
+    visit "/"
+    expect(page).to have_content "Log in"
   end
 end
