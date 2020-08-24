8ace7e77c62855a8b25c73397650b190cdf58ec9
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-08-24 12:03:39 +0100

    Validate that the start address is an ipv4 address
    Co-authored-by: Cait <caitlin@madetech.com>

--- app/models/subnet.rb
+++ app/models/subnet.rb
@@ -1,13 +1,21 @@
 class Subnet < ApplicationRecord
-  validate :cidr_block_is_a_valid_ipv4_subnet
+  validate :cidr_block_is_a_valid_ipv4_subnet, :start_address_is_a_valid_ipv4_address
 
   def ip_addr
     IPAddr.new(cidr_block)
   end
 
+  private
+
   def cidr_block_is_a_valid_ipv4_subnet
     unless IPAddress.valid_ipv4_subnet?(cidr_block)
       errors.add(:cidr_block, "is not a valid IPv4 subnet")
+    end
+  end
+
+  def start_address_is_a_valid_ipv4_address
+    unless IPAddress.valid_ipv4?(start_address)
+      errors.add(:start_address, "is not a valid IPv4 address")
     end
   end
 end
--- spec/models/subnet_spec.rb
+++ spec/models/subnet_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Subnet, type: :model do
     expect(subnet).not_to be_valid
     expect(subnet.errors[:cidr_block]).to eq(["is not a valid IPv4 subnet"])
   end
+
+  it "validates a correct start address" do
+    subnet = build :subnet, start_address: "10.0.4.1"
+    expect(subnet).to be_valid
+  end
+
+  it "validates an incorrect start address" do
+    subnet = build :subnet, start_address: "10.0.4"
+    expect(subnet).not_to be_valid
+    expect(subnet.errors[:start_address]).to eq(["is not a valid IPv4 address"])
+  end
 end
