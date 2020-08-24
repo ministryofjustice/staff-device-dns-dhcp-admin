696a86457385e0384392a4e58c81b63915cc9efa
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-08-24 11:48:47 +0100

    added validation for cidr blocks on subnet
    Co-authored-by: Cait <caitlin@madetech.com>

--- app/models/subnet.rb
+++ app/models/subnet.rb
@@ -1,5 +1,13 @@
 class Subnet < ApplicationRecord
+  validate :cidr_block_is_a_valid_ipv4_subnet
+
   def ip_addr
     IPAddr.new(cidr_block)
+  end
+
+  def cidr_block_is_a_valid_ipv4_subnet
+    unless IPAddress.valid_ipv4_subnet?(cidr_block)
+      errors.add(:cidr_block, "is not a valid IPv4 subnet")
+    end
   end
 end
--- spec/models/subnet_spec.rb
+++ spec/models/subnet_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Subnet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates a correct CIDR block" do
+    subnet = build :subnet, cidr_block: "10.0.4.0/24"
+    expect(subnet).to be_valid
+  end
+
+  it "validates an incorrect CIDR block" do
+    subnet = build :subnet, cidr_block: "10.0.4.0/abcd"
+    expect(subnet).not_to be_valid
+    expect(subnet.errors[:cidr_block]).to eq(["is not a valid IPv4 subnet"])
+  end
 end
