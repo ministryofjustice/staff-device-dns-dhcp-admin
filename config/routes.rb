ffadc313a0ddebfcd8c62215fbf836a70c718b92
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-08-27 17:50:21 +0100

    Add sign out link via delete method to the navbar
    This changes sets the sign_out_via to [:get, :delete]. With this we can use
    a link_to with method: :delete but also fall back to a get request if javascript is
    disabled.
    
    The Govuk design system has decided to not implement 'link buttons'
    (see https://github.com/alphagov/govuk-design-system-backlog/issues/159).
    This would have meant we could use a link button in the header and only used the
    delete method to sign out.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   devise_scope :user do
     get "sign_in", to: "devise/sessions#new", as: :new_user_session
-    get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+    match "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session, via: [:get, :delete]
   end
 
   resources :subnets, only: [:index, :new, :create, :edit, :update]
