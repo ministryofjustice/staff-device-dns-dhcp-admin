2553f27f77d20e67fb433dabc2e663d689673918
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-09-08 09:26:12 +0100

    Add ability to create zones

---
+++ spec/acceptance/create_zones_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe "create zones", type: :feature do
+  before do
+    login_as User.create
+  end
+
+  it "creates a new zone" do
+    visit "/zones"
+
+    click_on "Create a new zone"
+
+    expect(current_path).to eql("/zones/new")
+
+    fill_in "Name", with: "test.example.com"
+    fill_in "Forwarders", with: "10.1.1.25;10.1.1.28;"
+    fill_in "Purpose", with: "Frontend Driven Test"
+
+    click_button "Create"
+
+    expect(current_path).to eq("/zones")
+
+    zone = Zone.last
+    expect(zone.name).to eq "test.example.com"
+    expect(zone.forwarders).to eq "10.1.1.25;10.1.1.28;"
+    expect(zone.purpose).to eq "Frontend Driven Test"
+  end
+
+  it "displays error if form cannot be submitted" do
+    visit "/zones/new"
+
+    click_button "Create"
+
+    expect(page).to have_content "There is a problem"
+  end
+end
