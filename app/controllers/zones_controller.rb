a304266cd6971f8f9ff910a0355eba00e40a91ab
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-09-08 09:26:12 +0100

    Add ability to create zones

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -2,4 +2,23 @@ class ZonesController < ApplicationController
   def index
     @zones = Zone.select(:name, :forwarders, :purpose).all
   end
+
+  def new
+    @zone = Zone.new
+  end
+
+  def create
+    @zone = Zone.new(zone_params)
+    if @zone.save
+      redirect_to zones_path, notice: "Successfully created zone"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def zone_params
+    params.require(:zone).permit(:name, :forwarders, :purpose)
+  end
 end
