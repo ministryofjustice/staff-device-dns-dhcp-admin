a4249a4a124e34e9f93df115ac2692f39878ebbd
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-09-10 16:18:37 +0100

    Temporariliy set all users to editor until we pull the roles from cognito

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: Rails.env.development? ? %i[cognito developer] : %i[cognito]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.editor = true # Temporarily set all to editor until we pull roles from cognito
+    end
   end
 end
