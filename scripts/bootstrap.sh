8438b147f05705d5aa1879e51b091d86d3c39e8e
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-09-22 16:15:42 +0100

    Connect to DB using SSL before altering user to require SSL
    This ensures that any subsequent runs of this script will not fail because
    the user already requires SSL.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,4 +2,4 @@
 
 set -v -e -u -o pipefail
 
-mysql -u ${DB_USER} -p${DB_PASS} -n ${DB_NAME} -h ${DB_HOST} -e "ALTER USER '${DB_USER}'@'%' REQUIRE SSL;"
+mysql -u ${DB_USER} -p${DB_PASS} -n ${DB_NAME} -h ${DB_HOST} --ssl-ca=/usr/src/cert/rds-combined-ca-bundle.pem --ssl-mode=VERIFY_IDENTITY -e "ALTER USER '${DB_USER}'@'%' REQUIRE SSL;"
