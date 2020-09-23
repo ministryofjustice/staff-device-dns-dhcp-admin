781c0867631e91f7a6f11e2f499528164eb89470
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-09-23 10:32:55 +0100

    Run bootstrap script as an ecs task
    The previous iteration was failing to connect to the database

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,4 +2,28 @@
 
 set -v -e -u -o pipefail
 
-mysql -u ${DB_USER} -p${DB_PASS} -n ${DB_NAME} -h ${DB_HOST} --ssl-ca=/usr/src/cert/rds-combined-ca-bundle.pem --ssl-verify-server-cert -e "ALTER USER '${DB_USER}'@'%' REQUIRE SSL;"
+source ./scripts/aws-helpers.sh
+
+function require_ssl() {
+  local require_ssl_command="mysql -u ${DB_USER} -p${DB_PASS} -n ${DB_NAME} -h ${DB_HOST} --ssl-ca=/usr/src/cert/rds-combined-ca-bundle.pem --ssl-verify-server-cert -e \"ALTER USER '${DB_USER}'@'%' REQUIRE SSL;\""
+  local docker_service_name="admin"
+  local cluster_name service_name task_definition docker_service_name
+
+  cluster_name="staff-device-${ENV}-dhcp-admin-cluster"
+  service_name="staff-device-${ENV}-dhcp-admin"
+  task_definition="staff-device-${ENV}-dhcp-admin-task"
+
+  echo "${cluster_name}"
+  echo "${service_name}"
+  aws sts get-caller-identity
+  echo "===================================================================================================="
+
+  run_task_with_command \
+    "${cluster_name}" \
+    "${service_name}" \
+    "${task_definition}" \
+    "${docker_service_name}" \
+    "${require_ssl_command}"
+}
+
+require_ssl
