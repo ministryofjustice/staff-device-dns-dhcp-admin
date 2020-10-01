6c810401950a09f9978333b09fadc6a0d971803b
Andy Mitchell <me@themitchell.co.uk>
2020-10-01 10:24:17 +0100

    Add test coverage metrics to test output

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+require "simplecov-console"
+SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start "rails"
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
