a8a9eb4cbcc9fc6d43b3d2893f685e64277551e2
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-10-02 11:25:19 +0100

    Refactor options

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,30 +1,23 @@
 class Option < ApplicationRecord
-  validates :routers, presence: true
-  validates :domain_name_servers, presence: true
+  validates :routers, presence: { message: "must contain at least one IPv4 address" }, ipv4_list: true
+  validates :domain_name_servers, presence: { message: "must contain at least one IPv4 address" }, ipv4_list: true
   validates :domain_name, presence: true
 
-  validate :routers_is_a_valid
-  validate :domain_name_servers_is_valid
-
-  def routers_is_a_valid
-    return if routers.blank?
-    validate_ip_addresses(:routers, routers)
+  def routers
+    return [] unless self[:routers]
+    self[:routers].split(",")
+  end
+  
+  def routers=(val)
+    self[:routers] = val.join(",") || ""
   end
 
-  def domain_name_servers_is_valid
-    return if domain_name_servers.blank?
-    validate_ip_addresses(:domain_name_servers, domain_name_servers)
+  def domain_name_servers
+    return [] unless self[:domain_name_servers]
+    self[:domain_name_servers].split(",")
   end
-
-  private
-
-  def all_ips_valid?(ip_addresses_string)
-    ip_addresses = ip_addresses_string.split(",")
-    ip_addresses.all? { |ip_address| IPAddress.valid_ipv4?(ip_address) }
-  end
-
-  def validate_ip_addresses(attribute_name, ip_addresses_string)
-    return if all_ips_valid?(ip_addresses_string)
-    errors.add(attribute_name, "contains an invalid IPv4 address")
+  
+  def domain_name_servers=(val)
+    self[:domain_name_servers] = val.join(",") || ""
   end
 end
---
+++ app/validators/ipv4_list_validator.rb
@@ -0,0 +1,7 @@
+class Ipv4ListValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, ip_addresses)
+    unless ip_addresses.all? { |ip_address| IPAddress.valid_ipv4?(ip_address) }
+      record.errors.add(attribute, "contains an invalid IPv4 address")
+    end
+  end
+end
