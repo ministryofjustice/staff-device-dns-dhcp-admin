3e6ea925e0832e5a07674bfd19a9d005e0b1a89b
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-10-02 11:25:19 +0100

    Refactor options

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -3,29 +3,33 @@ require "rails_helper"
 RSpec.describe Option, type: :model do
   subject { build :option }
 
-  it { is_expected.to validate_presence_of :routers }
-  it { is_expected.to validate_presence_of :domain_name_servers }
+  it "has a valid factory" do
+    expect(subject).to be_valid
+  end
+
+  it "is invalid with no routers" do
+    subject.routers = []
+    expect(subject).not_to be_valid
+    expect(subject.errors[:routers]).to include("must contain at least one IPv4 address")
+  end
+
+  it "is invalid with no domain_name_servers" do
+    subject.domain_name_servers = []
+    expect(subject).not_to be_valid
+    expect(subject.errors[:domain_name_servers]).to include("must contain at least one IPv4 address")
+  end
+
   it { is_expected.to validate_presence_of :domain_name }
 
   it "rejects an incorrect routers" do
-    option = build :option, routers: "abcd,efg"
+    option = build :option, routers: ["abcd","efg"]
     expect(option).not_to be_valid
     expect(option.errors[:routers]).to eq(["contains an invalid IPv4 address"])
   end
 
   it "rejects an incorrect domain_name_server" do
-    option = build :option, domain_name_servers: "abcd,efg"
+    option = build :option, domain_name_servers: ["abcd","efg"]
     expect(option).not_to be_valid
     expect(option.errors[:domain_name_servers]).to eq(["contains an invalid IPv4 address"])
-  end
-
-  it "validates some routers" do
-    option = build :option, routers: "10.0.3.1,10.0.3.3"
-    expect(option).to be_valid
-  end
-
-  it "validates some domain_name_servers" do
-    option = build :option, routers: "10.0.3.1,10.0.3.3"
-    expect(option).to be_valid
   end
 end
