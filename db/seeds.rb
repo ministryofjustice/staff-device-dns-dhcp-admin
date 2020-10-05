ae57d23def5aca412336d6a8f6ed17c15d54b4a0
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-10-05 08:43:05 +0100

    Added initial global options

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Option.find_or_create_by(global: true) do |option|
+  option.routers = ["192.1.1.10","192.1.1.12"]
+  option.domain_name_servers = ["192.1.2.10","192.1.2.12"]
+  option.domain_name = "seeds.example.com"
+end
