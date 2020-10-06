88a56f9ccd18e1e40c86ac937fea8db5a8767f02
Andy Mitchell <me@themitchell.co.uk>
2020-10-06 11:59:53 +0100

    Test option ip address getters and setters

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -32,4 +32,92 @@ RSpec.describe Option, type: :model do
     expect(option).not_to be_valid
     expect(option.errors[:domain_name_servers]).to eq(["contains an invalid IPv4 address"])
   end
+
+  describe "#routers" do
+    context "when routers is nil" do
+      before do
+        subject.routers = nil
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq([])
+      end
+    end
+
+    context "when routers is not empty" do
+      before do
+        subject.routers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq(["192.168.0.2","192.168.0.3"])
+      end
+    end
+  end
+
+  describe "#routers=" do
+    context "when the value is a string" do
+      before do
+        subject.routers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq(["192.168.0.2","192.168.0.3"])
+      end
+    end
+
+    context "when the value is an array" do
+      before do
+        subject.routers = ["192.168.0.2","192.168.0.3"]
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq(["192.168.0.2","192.168.0.3"])
+      end
+    end
+  end
+
+  describe "#domain_name_servers" do
+    context "when domain_name_servers is nil" do
+      before do
+        subject.domain_name_servers = nil
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq([])
+      end
+    end
+
+    context "when domain_name_servers is not empty" do
+      before do
+        subject.domain_name_servers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq(["192.168.0.2","192.168.0.3"])
+      end
+    end
+  end
+
+  describe "#domain_name_servers=" do
+    context "when the value is a string" do
+      before do
+        subject.domain_name_servers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq(["192.168.0.2","192.168.0.3"])
+      end
+    end
+
+    context "when the value is an array" do
+      before do
+        subject.domain_name_servers = ["192.168.0.2","192.168.0.3"]
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq(["192.168.0.2","192.168.0.3"])
+      end
+    end
+  end
 end
