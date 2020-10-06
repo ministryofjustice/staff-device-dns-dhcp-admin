0f9937bf0ba86ded3acbd330c304d1bc9e94f25f
Andy Mitchell <me@themitchell.co.uk>
2020-10-06 15:38:54 +0100

    Show subnet details on the subnet show page
    Also add links from the site show page, to view a subnet.
    
    Co-authored-by: Caitlin <caitlin@madetech.com>

---
+++ spec/acceptance/show_subnet_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe "showing a subnet", type: :feature do
+  context "when the user is unauthenticated" do
+    it "does not allow viewing subnets" do
+      visit "/subnets/nonexistant-subnet-id"
+
+      expect(page).to have_content "You need to sign in or sign up before continuing."
+    end
+  end
+
+  context "when the user is authenticated" do
+    before do
+      login_as User.create!
+    end
+
+    context "when the subnet exists" do
+      let(:subnet) { create :subnet }
+      let(:site) { subnet.site }
+
+      it "allows viewing subnets and its subnets" do
+        visit "/sites/#{site.to_param}"
+
+        click_on "View"
+
+        expect(page).to have_content subnet.cidr_block
+        expect(page).to have_content subnet.start_address
+        expect(page).to have_content subnet.end_address
+      end
+    end
+  end
+end
