0d3865eeddf103b761761351d51ced73b3cfdc54
Andy Mitchell <me@themitchell.co.uk>
2020-10-07 15:43:21 +0100

    Allow subnet options to be null
    When creating or updating options, only 1 is required, all the rest can be blank

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,13 +1,11 @@
 class Option < ApplicationRecord
   belongs_to :subnet
 
-  validates :routers,
-    presence: {message: "must contain at least one IPv4 address separated using commas"},
-    ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
-  validates :domain_name_servers,
-    presence: {message: "must contain at least one IPv4 address separated using commas"},
-    ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
-  validates :domain_name, presence: true
+  INVALID_IPV4_LIST_MESSAGE = "contains an invalid IPv4 address or is not separated using commas"
+  validates :routers, ipv4_list: {message: INVALID_IPV4_LIST_MESSAGE}
+  validates :domain_name_servers, ipv4_list: {message: INVALID_IPV4_LIST_MESSAGE}
+
+  validate :at_least_one_option
 
   def routers
     return [] unless self[:routers]
@@ -33,5 +31,12 @@ class Option < ApplicationRecord
     else
       val
     end
+  end
+
+  private
+
+  def at_least_one_option
+    return if routers.any? || domain_name_servers.any? || domain_name.present?
+    errors.add(:base, "At least one option must be filled out")
   end
 end
