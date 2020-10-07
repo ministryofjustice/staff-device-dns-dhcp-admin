a41f29411e9d98b12a08585f31cea2eaff81d55f
Andy Mitchell <me@themitchell.co.uk>
2020-10-07 14:30:31 +0100

    Fix autoreloading of files in development docker conatiner
    Autoreloading using the EventedFileUpdateChecker does not work in a
    containerized docker environment. Instead use the FileUpdateChecker.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   config.check_yarn_integrity = false
   config.s3_aws_config = {
