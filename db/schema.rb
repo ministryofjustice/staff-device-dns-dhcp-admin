2c323b95caad76075d12fe49b2229fd70d47bdcf
jivdhaliwal <jivdhaliwal@users.noreply.github.com>
2020-10-08 10:16:47 +0100

    Create GlobalOption model (#59)
    * Created GlobalOptions model
    
    Co-authored-by: Efua Akumanyi <efua.akumanyi@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_131618) do
+ActiveRecord::Schema.define(version: 2020_10_07_143425) do
+  create_table "global_options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "routers", null: false
+    t.string "domain_name_servers", null: false
+    t.string "domain_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "options", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "routers", null: false
     t.string "domain_name_servers", null: false
---
+++ spec/models/global_option_spec.rb
@@ -0,0 +1,121 @@
+require "rails_helper"
+
+RSpec.describe GlobalOption, type: :model do
+  subject { build :global_option }
+
+  it "has a valid factory" do
+    expect(subject).to be_valid
+  end
+
+  it do
+    is_expected.to validate_presence_of(:routers)
+      .with_message("must contain at least one IPv4 address separated using commas")
+  end
+
+  it do
+    is_expected.to validate_presence_of(:domain_name_servers)
+      .with_message("must contain at least one IPv4 address separated using commas")
+  end
+
+  it { is_expected.to validate_presence_of :domain_name }
+
+  it "rejects an incorrect routers" do
+    option = build :option, routers: ["abcd", "efg"]
+    expect(option).not_to be_valid
+    expect(option.errors[:routers]).to eq(["contains an invalid IPv4 address or is not separated using commas"])
+  end
+
+  it "rejects an incorrect domain_name_server" do
+    option = build :option, domain_name_servers: ["abcd", "efg"]
+    expect(option).not_to be_valid
+    expect(option.errors[:domain_name_servers]).to eq(["contains an invalid IPv4 address or is not separated using commas"])
+  end
+
+  describe "#routers" do
+    context "when routers is nil" do
+      before do
+        subject.routers = nil
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq([])
+      end
+    end
+
+    context "when routers is not empty" do
+      before do
+        subject.routers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq(["192.168.0.2", "192.168.0.3"])
+      end
+    end
+  end
+
+  describe "#routers=" do
+    context "when the value is a string" do
+      before do
+        subject.routers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq(["192.168.0.2", "192.168.0.3"])
+      end
+    end
+
+    context "when the value is an array" do
+      before do
+        subject.routers = ["192.168.0.2", "192.168.0.3"]
+      end
+
+      it "returns an empty array" do
+        expect(subject.routers).to eq(["192.168.0.2", "192.168.0.3"])
+      end
+    end
+  end
+
+  describe "#domain_name_servers" do
+    context "when domain_name_servers is nil" do
+      before do
+        subject.domain_name_servers = nil
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq([])
+      end
+    end
+
+    context "when domain_name_servers is not empty" do
+      before do
+        subject.domain_name_servers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq(["192.168.0.2", "192.168.0.3"])
+      end
+    end
+  end
+
+  describe "#domain_name_servers=" do
+    context "when the value is a string" do
+      before do
+        subject.domain_name_servers = "192.168.0.2,192.168.0.3"
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq(["192.168.0.2", "192.168.0.3"])
+      end
+    end
+
+    context "when the value is an array" do
+      before do
+        subject.domain_name_servers = ["192.168.0.2", "192.168.0.3"]
+      end
+
+      it "returns an empty array" do
+        expect(subject.domain_name_servers).to eq(["192.168.0.2", "192.168.0.3"])
+      end
+    end
+  end
+end
