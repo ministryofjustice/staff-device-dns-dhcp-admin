dca92b77d0a605212e9f2da7f82b1c186386b84b
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-08 10:00:30 +0100

    Add Ipv4 validation to GlobalOption
    Co-authored-by: Efua Akumanyi <efua.akumanyi@gmail.com>

--- app/models/global_option.rb
+++ app/models/global_option.rb
@@ -1,3 +1,35 @@
 class GlobalOption < ApplicationRecord
-  validates :routers, :domain_name_servers, :domain_name, presence: true
+  validates :routers,
+    presence: {message: "must contain at least one IPv4 address separated using commas"},
+    ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
+  validates :domain_name_servers,
+    presence: {message: "must contain at least one IPv4 address separated using commas"},
+    ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
+  validates :domain_name, presence: true
+
+  def routers
+    return [] unless self[:routers]
+    self[:routers].split(",")
+  end
+
+  def routers=(val)
+    self[:routers] = if val.respond_to?(:join)
+      val.join(",")
+    else
+      val
+    end
+  end
+
+  def domain_name_servers
+    return [] unless self[:domain_name_servers]
+    self[:domain_name_servers].split(",")
+  end
+
+  def domain_name_servers=(val)
+    self[:domain_name_servers] = if val.respond_to?(:join)
+      val.join(",")
+    else
+      val
+    end
+  end
 end
