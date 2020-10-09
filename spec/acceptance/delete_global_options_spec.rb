f4f7100b1aee873eaad807e6874187624acfdbf4
Efua Akumanyi <efua.akumanyi@gmail.com>
2020-10-09 12:01:53 +0100

    Add delete acceptance test for global options

---
+++ spec/acceptance/delete_global_options_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "delete gobal options", type: :feature do
+  before do
+    login_as User.create!(editor: true)
+  end
+
+  it "delete global options" do
+    global_option = create :global_option
+    visit "/global_options"
+
+    click_on "Delete global options"
+
+    expect(page).to have_content("Are you sure you want to delete these global options?")
+
+    click_on "Delete global options"
+
+    expect(page).to have_content("Successfully deleted global options")
+    expect(page).not_to have_content(global_option.domain_name)
+  end
+end
