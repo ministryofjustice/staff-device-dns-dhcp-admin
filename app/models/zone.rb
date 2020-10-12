f8600de9eb549f0e0574c916536d629d79fbcdf8
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-12 14:56:38 +0100

    Validate that the Zone domain name is correctly formatted

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,5 +1,6 @@
 class Zone < ApplicationRecord
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, uniqueness: {case_sensitive: false},
+                   format: {with: /\A[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/}
   validates :forwarders,
     presence: {message: "must contain at least one IPv4 address separated using commas"},
     ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
