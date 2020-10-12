a0f50e19cf08677130bb88479646afcc7532b98f
Andy Mitchell <me@themitchell.co.uk>
2020-10-12 14:09:21 +0100

    Addd basic navigation menu
    Add navigation menu to the header bar linking to top level DNS / DHCP
    pages. At this point a top level config menu seems overkill as does
    a dropdown subnav. For now simply link to global config from the top
    level entry point for namespaces.
    
    In the future we likely want to discuss scoping routes under dns/dhcp
    as each will have its own set of global configurations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def dhcp
+  end
+
+  def dns
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def field_error(resource, key)
     resource&.errors&.include?(key.to_sym) ? "govuk-form-group--error" : ""
   end
+
+  def selected_class_if_controller(controller_names)
+    if [controller_names].flatten.include?(controller_name)
+      return ' govuk-header__navigation-item--active'
+    end
+  end
 end
