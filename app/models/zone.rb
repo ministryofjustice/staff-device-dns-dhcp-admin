f34c00a577f54c8c105e88eb24c8b20d3375b22b
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-13 11:47:26 +0100

    Update domain name validation to include uppercase characters

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,6 +1,6 @@
 class Zone < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false},
-                   format: {with: /\A[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/}
+                   format: {with: /\A[a-zA-Z\d\-]+(\.[a-zA-Z\d\-]+)*\.[a-z]+\z/}
   validates :forwarders,
     presence: {message: "must contain at least one IPv4 address separated using commas"},
     ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
