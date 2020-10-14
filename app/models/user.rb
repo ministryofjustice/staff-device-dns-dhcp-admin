1c20527140dc392481739dac1065fa293f4b2d2f
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-14 10:54:09 +0100

    Timeout user session after 5 minutes of inactivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   EDITOR_ROLE = "editor"
 
-  devise :omniauthable, omniauth_providers: Rails.env.development? ? %i[cognito developer] : %i[cognito]
+  devise :omniauthable, :timeoutable,
+    omniauth_providers: (Rails.env.development? ? %i[cognito developer] : %i[cognito])
 
   def self.from_omniauth(auth)
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
