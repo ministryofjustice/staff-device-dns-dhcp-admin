9867955455717008907f01158a064d263366393d
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-14 12:10:56 +0100

    Add devise trackable module to Users
    Allows us to track when the user first signed in and implement a hard session
    timeout after 12 hours

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   EDITOR_ROLE = "editor"
 
-  devise :omniauthable, :timeoutable,
-    omniauth_providers: (Rails.env.development? ? %i[cognito developer] : %i[cognito])
+  devise :omniauthable, :timeoutable, :trackable,
+         omniauth_providers: (Rails.env.development? ? %i[cognito developer] : %i[cognito])
+
 
   def self.from_omniauth(auth)
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
@@ -16,5 +17,11 @@ class User < ApplicationRecord
     user.editor = true
     user.save
     user
+  end
+
+  protected
+
+  def extract_ip_from(request)
+    # Skip IP logging in Trackable module
   end
 end
