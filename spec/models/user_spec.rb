bbd8d35f4ff3fb53c4bdf3f01277c271bc98701e
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-21 11:57:41 +0100

    Test that the email is set correctly through User.from_omniauth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 describe User, type: :model do
   describe ".from_omniauth" do
     subject(:user) { User.from_omniauth(auth_hash) }
+    let(:role) { "" }
     let(:auth_hash) do
       # Mocking OmniAuth::AuthHash
       double(provider: "cognito", uid: "1",
-             extra: double(raw_info: {"custom:app_role" => role}))
+             extra: double(raw_info: {"custom:app_role" => role, :identities => [double(userId: "test_from_omniauth@example.com")]}))
     end
 
     context "editor app role" do
@@ -23,6 +24,10 @@ describe User, type: :model do
       it "sets editor to false" do
         expect(user.editor?).to eq false
       end
+    end
+
+    it "sets the email correctly" do
+      expect(user.email).to eq "test_from_omniauth@example.com"
     end
   end
 end
