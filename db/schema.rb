25c3091e4343abe61eac4da92deaa420bd45fd13
Efua Akumanyi <64264510+efuaakum@users.noreply.github.com>
2020-10-21 11:04:54 +0100

    Feature/pttp 1430 add email to logs (#81)
    * Add user email to audit logs
    
    Co-authored-by: Caitlin <caitlin@madetech.com>
    
    * Set user email to userId in auth token
    
    * Commit changes made by code formatters
    
    * Reinstate removed subnets/options relationship
    
    Co-authored-by: Caitlin <caitlin@madetech.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_101903) do
+ActiveRecord::Schema.define(version: 2020_10_19_152742) do
   create_table "audits", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_101903) do
     t.string "provider"
     t.string "uid"
     t.boolean "editor", default: false
+    t.string "email"
   end
 
   create_table "zones", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
