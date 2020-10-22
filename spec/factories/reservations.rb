05710be726c5fff9c3e03c3c33f5155e26bc46dd
Efua Akumanyi <64264510+efuaakum@users.noreply.github.com>
2020-10-22 16:31:20 +0100

    Add update to reservation factory (#88)

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :reservation do
-    subnet factory: :subnet, cidr_block: "192.0.2.1/24"
+    subnet factory: :subnet, start_address: "192.0.2.1", end_address: "192.0.2.255", cidr_block: "192.0.2.1/24"
     hw_address { "01:bb:cc:dd:ee:ff" }
     ip_address { "192.0.2.1" }
     hostname { "test.example.com" }
