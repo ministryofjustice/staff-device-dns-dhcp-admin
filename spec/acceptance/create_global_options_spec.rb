b4d1a099aea613806bff59a24d1a90415d6bbf34
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-23 12:04:25 +0100

    Add valid lifetime to global options form

--- spec/acceptance/create_global_options_spec.rb
+++ spec/acceptance/create_global_options_spec.rb
@@ -40,6 +40,7 @@ describe "create global options", type: :feature do
       fill_in "Routers", with: "10.0.1.0,10.0.1.2"
       fill_in "Domain name servers", with: "10.0.2.1,10.0.2.2"
       fill_in "Domain name", with: "test.example.com"
+      fill_in "Valid lifetime", with: 12345
 
       click_on "Create"
 
@@ -47,6 +48,7 @@ describe "create global options", type: :feature do
       expect(page).to have_content("10.0.1.0,10.0.1.2")
       expect(page).to have_content("10.0.2.1,10.0.2.2")
       expect(page).to have_content("test.example.com")
+      expect(page).to have_content("12345")
 
       click_on "Audit log"
 
