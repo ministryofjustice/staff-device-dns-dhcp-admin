ea9e1b686e124d8c8fdc962521b9d8d6c18e4298
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-30 09:43:18 +0000

    Remove routers and domain name server setters from option
    These setters, which allowed us to pass in an array when setting the routers and
    domain_name_servers, were only ever used in tests

--- spec/factories/options.rb
+++ spec/factories/options.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :option do
     subnet
-    routers { ["10.0.4.0", "10.0.4.2"] }
-    domain_name_servers { ["12.0.4.1", "12.0.4.5"] }
+    routers { "10.0.4.0,10.0.4.2" }
+    domain_name_servers { "12.0.4.1,12.0.4.5" }
     domain_name { "www.examgitple.com" }
   end
 end
