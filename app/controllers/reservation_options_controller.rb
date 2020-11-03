4db0f2de96836a7ac7157c4baa48e83772dcd96d
cait <caitlin@madetech.com>
2020-11-03 12:06:07 +0000

    Add destroy functionality for reservation options

--- app/controllers/reservation_options_controller.rb
+++ app/controllers/reservation_options_controller.rb
@@ -1,6 +1,6 @@
 class ReservationOptionsController < ApplicationController
-  before_action :set_reservation
-  before_action :set_reservation_option
+  before_action :set_reservation, except: [:destroy]
+  before_action :set_reservation_option, only: [:destroy]
 
   def new
     @reservation_option = @reservation.build_reservation_option
@@ -19,6 +19,21 @@ class ReservationOptionsController < ApplicationController
     end
   end
 
+  def destroy
+    authorize! :destroy, @reservation_option
+    if confirmed?
+      if @reservation_option.destroy
+        # publish_kea_config
+        # deploy_dhcp_service
+        redirect_to reservation_path(@reservation_option.reservation), notice: "Successfully deleted reservation options"
+      else
+        redirect_to reservation_path(@reservation_option.reservation), error: "Failed to delete the reservation options"
+      end
+    else
+      render "destroy"
+    end
+  end
+
   private
 
   def reservation_id
@@ -30,10 +45,18 @@ class ReservationOptionsController < ApplicationController
   end
 
   def set_reservation_option
-    @reservation_option = @reservation.reservation_option
+    @reservation_option = ReservationOption.find(reservation_option_id)
+  end
+
+  def reservation_option_id
+    params.fetch(:id)
   end
 
   def reservation_option_params
     params.require(:reservation_option).permit(:routers, :domain_name)
+  end
+
+  def confirmed?
+    params.fetch(:confirm, false)
   end
 end
--- spec/factories/reservation_options.rb
+++ spec/factories/reservation_options.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :reservation_option do
-    domain_name { "example.com" }
+    domain_name { "example.test.com" }
     routers { "10.0.0.1,10.0.0.100" }
     reservation
   end
