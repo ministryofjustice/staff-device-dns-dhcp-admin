5ae6c730fe8929b8eeaa232c3dd333e0556a6e8a
Andy Mitchell <me@themitchell.co.uk>
2020-11-05 15:55:48 +0000

    Use fetch to reference KEA_CONTROL_AGENT_URI

--- app/lib/gateways/kea_control_agent.rb
+++ app/lib/gateways/kea_control_agent.rb
@@ -4,7 +4,7 @@ require "json"
 module Gateways
   class KeaControlAgent
     def fetch_leases
-      uri = URI(ENV["KEA_CONTROL_AGENT_URI"])
+      uri = URI(ENV.fetch('KEA_CONTROL_AGENT_URI'))
       http = Net::HTTP.new(uri.host, uri.port)
       req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json")
       req.body = {
