9e149b995d4db9c3e892acf79be5b1139c050997
Joshua-Luke Bevan <63861228+jbevan4@users.noreply.github.com>
2020-11-05 09:55:06 +0000

    Added domain validation (#111)
    * Added domain validation
    
    * changed text lookup for unhappy path

--- app/models/reservation_option.rb
+++ app/models/reservation_option.rb
@@ -5,6 +5,9 @@ class ReservationOption < ApplicationRecord
   validates :reservation, presence: true
   validates :routers, ipv4_list: {message: INVALID_IPV4_LIST_MESSAGE}
   validates :domain_name, domain_name: true
+
+  validates_format_of :domain_name, with: /\A(http|https):\/\/|[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?\z/ix, message: "contains an invalid domain name", allow_nil: true
+
   validate :at_least_one_option
 
   audited
