db79db6d75b984f8f6532ff417e88f87d7da6ec4
Emile Swarts <emile.swarts123@gmail.com>
2020-11-30 12:37:42 +0000

    Allow * values in domain name (#158)
    * Allow * values in domain name
    
    We were using a built in domain name validator that rejects any * values
    required to set up the zones in BIND.
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,6 +1,9 @@
 class Zone < ApplicationRecord
-  validates :name, presence: true, uniqueness: {case_sensitive: false},
-                   domain_name: true
+  VALID_ZONE_REGEX = /\A[a-z0-9*]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates_format_of :name, with: VALID_ZONE_REGEX
+
   validates :forwarders,
     presence: {message: "must contain at least one IPv4 address separated using commas"},
     ipv4_list: {message: "contains an invalid IPv4 address or is not separated using commas"}
