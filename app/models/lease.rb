92874fe9d60467bd863546309653922a57510686
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-12-15 12:56:52 +0000

    Display lease state in the leases table
    Display the state (Leased, Declined, Expired / Reclaimed),
    so that it's clear what state a Lease is in
    
    This is important because a lease that has expired / is being reclaimed will
    be included in the response until it has been released / re-assigned.
    
    States are defined here: https://kea.readthedocs.io/en/kea-1.8.0/arm/hooks.html#the-lease4-add-lease6-add-commands

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,11 +1,21 @@
 class Lease
   attr_reader :hw_address,
     :ip_address,
-    :hostname
+    :hostname,
+    :state
 
-  def initialize(hw_address:, ip_address:, hostname:)
+  def initialize(hw_address:, ip_address:, hostname:, state:)
     @hw_address = hw_address
     @ip_address = ip_address
     @hostname = hostname
+    @state = state
+  end
+
+  def pretty_state
+    return "Leased" if state == 0
+    return "Declined" if state == 1
+    return "Expired / Reclaimed" if state == 2
+
+    "Unknown"
   end
 end
