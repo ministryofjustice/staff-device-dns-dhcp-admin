8794c5aa44d62f0ad922c24247ea19b30b8bc3bd
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2021-01-11 14:32:32 +0000

    Revert "Benchmark config publishing steps"
    This reverts commit 1c77f98c0aefe0be5e5c873e6bfdf9160a7e2fec.

--- app/lib/use_cases/verify_kea_config.rb
+++ app/lib/use_cases/verify_kea_config.rb
@@ -1,25 +1,19 @@
 module UseCases
   class VerifyKeaConfig
-    include ActiveSupport::Benchmarkable
-
-    def initialize(kea_control_agent_gateway:, logger: nil)
+    def initialize(kea_control_agent_gateway:)
       @kea_control_agent_gateway = kea_control_agent_gateway
-      @logger = logger
     end
 
     def call(config)
-      benchmark "Benchmark: UseCases::VerifyKeaConfig", level: :debug do
-        kea_control_agent_gateway.verify_config(config)
-        Result.new
-      rescue Gateways::KeaControlAgent::InternalError => error
-        Result.new(error)
-      end
+      kea_control_agent_gateway.verify_config(config)
+      Result.new
+    rescue Gateways::KeaControlAgent::InternalError => error
+      Result.new(error)
     end
 
     private
 
-    attr_reader :kea_control_agent_gateway,
-      :logger
+    attr_reader :kea_control_agent_gateway
 
     class Result
       attr_reader :error
