0aee6279ac3dff43453b4a5daaa54d790fb7f551
Andy Mitchell <me@themitchell.co.uk>
2021-01-14 15:11:58 +0000

    DEbug pwd in bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,6 +5,7 @@ set -v -e -u -o pipefail
 source ./scripts/aws_helpers.sh
 
 function require_ssl() {
+  echo "$(pwd)"
   local require_ssl_command="mysql -u ${DB_USER} -p${DB_PASS} -n ${DB_NAME} -h ${DB_HOST} --ssl-ca=../../cert/rds-combined-ca-bundle.pem -e \"ALTER USER '${DB_USER}'@'%' REQUIRE SSL;\""
   local docker_service_name="admin"
   local cluster_name service_name task_definition docker_service_name
