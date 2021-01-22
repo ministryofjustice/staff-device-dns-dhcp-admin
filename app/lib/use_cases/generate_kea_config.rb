5e413b6b79be270fc97d7cccc06c2fb4c790c863
Efua Akumanyi <64264510+efuaakum@users.noreply.github.com>
2021-01-22 11:13:02 +0000

    Increase HA  config values to defaults (#233)
    Keys set to [defaults](https://kea.readthedocs.io/en/kea-1.8.2/arm/hooks.html#load-balancing-configuration)
    
    Co-authored-by: Neil Kidd <neil.kidd@madetech.com>

--- app/lib/use_cases/generate_kea_config.rb
+++ app/lib/use_cases/generate_kea_config.rb
@@ -185,9 +185,9 @@ module UseCases
                   {
                     "this-server-name": "<SERVER_NAME>",
                     "mode": "hot-standby",
-                    "heartbeat-delay": 5000,
-                    "max-response-delay": 5000,
-                    "max-ack-delay": 5000,
+                    "heartbeat-delay": 10000,
+                    "max-response-delay": 60000,
+                    "max-ack-delay": 10000,
                     "max-unacked-clients": 0,
                     "peers": [
                       {
