23a44aba5f1d45db1ad6566959f5520e98520ae7
Dan Wilson <daniel.wilson4@justice.gov.uk>
2021-07-27 16:10:54 +0100

    add test for confirming shared networks are deleted

--- spec/acceptance/update_subnet_extension_spec.rb
+++ spec/acceptance/update_subnet_extension_spec.rb
@@ -31,4 +31,32 @@ describe "updating a subnet extension", type: :feature do
 
     expect_audit_log_entry_for(editor.email, "update", "Subnet")
   end
+
+  it "deletes the original shared network when assigned a new one" do
+    subnet = Audited.audit_class.as_user(editor) { create :subnet }
+    other_subnet = Audited.audit_class.as_user(editor) do
+      create :subnet, shared_network: create(:shared_network, site: subnet.shared_network.site)
+    end
+
+    visit "/subnets/#{subnet.to_param}"
+
+    click_on "Extend this network with another subnet"
+
+    select other_subnet.cidr_block, from: "Subnet"
+
+    expect_config_to_be_verified
+    expect_config_to_be_published
+
+    
+    expect(subnet.shared_network).not_to eq(other_subnet.shared_network)
+
+    click_button "Add to shared network"
+    
+    expect(SharedNetwork.find_by(id: other_subnet.shared_network.id)).to eq(nil)
+
+    other_subnet.reload
+    subnet.reload
+    expect(subnet.shared_network).to eq(other_subnet.shared_network)
+  end
+  
 end
