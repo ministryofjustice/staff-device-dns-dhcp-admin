1aa7a604cca1eb133d599f70fc50890f89bafc06
Dan Wilson <daniel.wilson4@justice.gov.uk>
2021-09-14 16:42:17 +0100

    Add terminal output for friendly feedback

--- scripts/config-parser/bin/dhcp_config_parser.rb
+++ scripts/config-parser/bin/dhcp_config_parser.rb
@@ -2,6 +2,9 @@
 
 require_relative "../lib/dhcp_config_parser"
 
+puts "There are #{DhcpConfigParser.run.length} differences in reservation data"
+puts "Take a look at ./data/reservation_diff.json for a breakdown"
+
 reservation_diff = File.open("./data/reservation_diff.json", "w")
 
 reservation_diff << DhcpConfigParser.run.to_json
