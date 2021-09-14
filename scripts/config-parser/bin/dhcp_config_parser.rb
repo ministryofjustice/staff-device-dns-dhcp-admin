53bd97bbf2f9df08ee787bb69c6a410de6fee33d
Daniel Wilson <68431297+wanieldilson@users.noreply.github.com>
2021-09-14 14:03:02 +0100

    Update scripts/config-parser/bin/dhcp_config_parser.rb
    Co-authored-by: Richard Race <1273965+richrace@users.noreply.github.com>

--- scripts/config-parser/bin/dhcp_config_parser.rb
+++ scripts/config-parser/bin/dhcp_config_parser.rb
@@ -4,6 +4,5 @@ require_relative "../lib/dhcp_config_parser"
 
 reservation_diff = File.open("./data/reservation_diff.json", "w")
 
-reservation_diff <<
-  DhcpConfigParser.run.to_json
+reservation_diff << DhcpConfigParser.run.to_json
 reservation_diff.close
