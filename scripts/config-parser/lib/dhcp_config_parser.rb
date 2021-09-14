d4ae35e99568af991cd9c2044c0826b94a142c18
Dan Wilson <daniel.wilson4@justice.gov.uk>
2021-09-14 12:22:17 +0100

    All files for the dhcp_config_parser

--- scripts/config-parser/lib/dhcp_config_parser.rb
+++ scripts/config-parser/lib/dhcp_config_parser.rb
@@ -1,27 +1,33 @@
 require "json"
 
 class DhcpConfigParser
+  KEA_CONFIG_FILEPATH = "./data/kea.json".freeze
+  LEGACY_CONFIG_FILEPATH = "./data/export.txt".freeze
 
+  def self.run
+    throw StandardError unless kea_config_exists?
+    throw StandardError unless export_file_exists?
+
+    # Populate these with data from the portal/export before running. 
+    # See readme if you're feeling ¯\_(ツ)_/¯
+    shared_network_id = "FITS_####" 
+    subnet_list = ["192.168.1.0", "192.168.2.0", "192.168.3.0"]
+
+    compared_reservations = find_missing_reservations(
+      kea_reservations: get_kea_reservations(shared_network_id, File.read(KEA_CONFIG_FILEPATH)),
+      legacy_reservations: get_legacy_reservations(File.read(LEGACY_CONFIG_FILEPATH), subnet_list)
+    )
+  end 
+  
   def self.kea_config_exists?
-
-    if !File.exist?("kea.json")
-      false
-    elsif File.exist?("kea.json")
-      true
-
-    end  
+    File.exist?(KEA_CONFIG_FILEPATH)
   end
 
   def self.export_file_exists?
-    if !File.exist?("export.txt")
-      false
-    elsif File.exist?("export.txt")
-      true
-    end
+    File.exist?(LEGACY_CONFIG_FILEPATH)
   end
 
-  def self.get_kea_reservations(shared_network_id,kea_config)
-    
+  def self.get_kea_reservations(shared_network_id, kea_config)
     kea_config_hash = JSON.parse(kea_config)
     shared_networks = kea_config_hash["Dhcp4"]["shared-networks"].select { |shared_network| shared_network["name"].include?(shared_network_id) }
 
@@ -30,14 +36,13 @@ class DhcpConfigParser
         # accumulator += (subnet["reservations"] || [])
         accumulator += subnet["reservations"] if subnet["reservations"]
       end
-    
+      
       accumulator
     end
 
   end
 
   def self.get_legacy_reservations(export, subnet_list)
-      
     reservation_fields = ["ip-address", "hw-address", "hostname"]
     legacy_reservations = []
 
@@ -50,8 +55,65 @@ class DhcpConfigParser
     end
     
     legacy_reservations.map { |row| reservation_fields.zip(row).to_h }
-
   end
+
+  def self.compare_reservations(kea_reservations:, legacy_reservations:)
+    compared_resverations = kea_reservations.map do |kea_reservation|
+      hw_address = kea_reservation["hw-address"].downcase.tr(':', '')
+
+      found_legacy = legacy_reservations.detect do |legacy_reservation|
+        legacy_reservation['hw-address'].downcase.tr(':', '') == hw_address
+      end 
+
+      {
+        "hw-address" => hw_address, 
+        "kea" => kea_reservation,
+        "legacy" => found_legacy
+      }
+    end 
+
+    compared_resverations += legacy_reservations.map do |legacy_reservation|
+      hw_address = legacy_reservation["hw-address"].downcase.tr(':', '')
+
+      found_kea = kea_reservations.detect do |kea_reservation|
+        kea_reservation['hw-address'].downcase.tr(':', '') == hw_address
+      end 
+
+      {
+        "hw-address" => hw_address, 
+        "legacy" => legacy_reservation,
+        "kea" => found_kea
+      }
+    end 
+
+    compared_resverations.reject do |reservation|
+      reservation["kea"] && reservation["legacy"]
+      # place to look into return IP == IP 
+    end 
+  end
+
+  def self.find_missing_reservations(kea_reservations:, legacy_reservations:) 
+    grouped_kea_reservations = kea_reservations.group_by { |res| res["hw-address"].downcase.tr(':', '') }
+    grouped_legacy_reservations = legacy_reservations.group_by { |res| res["hw-address"].downcase.tr(':', '') }
+
+    reservations = {}
+    
+    grouped_kea_reservations.each do |hw_address, grouped_kea_reservations|
+      reservations[hw_address] ||= { "hw-address" => hw_address, "kea" => nil, "legacy" => nil }
+      reservations[hw_address]["kea"] = grouped_kea_reservations.first 
+    end 
+
+    grouped_legacy_reservations.each do |hw_address, grouped_legacy_reservations|
+      reservations[hw_address] ||= { "hw-address" => hw_address, "kea" => nil, "legacy" => nil }
+      reservations[hw_address]["legacy"] = grouped_legacy_reservations.first 
+    end 
+
+    reservations.values.reject do |reservation|
+      reservation["kea"] && reservation["legacy"]
+      # place to look into return IP == IP 
+    end
+  end 
+
 end
 
 # 1. brittle / not flexible 
