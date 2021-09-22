ec40655e5665b92382b7095ec257f13c9161dfbd
Richard Race <1273965+richrace@users.noreply.github.com>
2021-09-22 12:29:04 +0100

    ✅  Update Acceptance test for viewing HW address

--- spec/acceptance/show_reservation_spec.rb
+++ spec/acceptance/show_reservation_spec.rb
@@ -28,5 +28,25 @@ describe "showing a reservation", type: :feature do
         expect(page).to have_content reservation.hostname
       end
     end
+
+    context "when the reservation exists with an incorrect formatted HW address" do
+      let(:reservation) do
+        res = build(:reservation, hw_address: "01-bb-cc-dd-ee-ff")
+        res.save(validate: false)
+        res
+      end
+      let(:subnet) { reservation.subnet }
+
+      it "allows viewing reservations and its options" do
+        visit "/subnets/#{subnet.to_param}"
+
+        click_on "View"
+
+        expect(page).not_to have_content reservation.hw_address
+        expect(page).to have_content "01:bb:cc:dd:ee:ff"
+        expect(page).to have_content reservation.ip_address
+        expect(page).to have_content reservation.hostname
+      end
+    end
   end
 end
