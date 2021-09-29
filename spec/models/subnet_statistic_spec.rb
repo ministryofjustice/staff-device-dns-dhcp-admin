c8692fc5a428c7e24ec9fc55741bb8839cd7c1b1
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-09-29 17:09:01 +0100

    rspec tests for the subnet_statistic model

---
+++ spec/models/subnet_statistic_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe SubnetStatistic do
+  let(:subnet) do 
+    build :subnet, 
+      start_address: "192.168.0.11", 
+      end_address: "192.168.0.20"
+  end
+  subject { described_class.new(subnet: subnet, leases: leases) }
+
+  describe "#num_remaining_ips" do
+    context "when there are no leases" do
+      let(:leases) { [] }
+
+      it "returns 10" do
+        expect(subject.num_remaining_ips).to eql(10)
+      end
+    end
+
+    context "when there are leases" do
+      let(:lease) { instance_double(Lease) }
+      let(:leases) { [lease] }
+
+      it "returns 9" do
+        expect(subject.num_remaining_ips).to eql(9)
+      end
+    end
+  end
+end
