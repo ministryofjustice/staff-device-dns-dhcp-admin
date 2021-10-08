076d2a449c457e8b8edb31f35454452090d53851
Dan Wilson <daniel.wilson4@justice.gov.uk>
2021-10-08 11:00:18 +0100

    :recycle: bring entry point script in line with refactor

--- scripts/config-parser/bin/dhcp_config_parser.rb
+++ scripts/config-parser/bin/dhcp_config_parser.rb
@@ -2,7 +2,7 @@
 
 require_relative "../lib/dhcp_config_parser"
 
-parsed_config = DhcpConfigParser.run
+parsed_config = DhcpConfigParser.new(kea_config_filepath: "./data/kea.json").run
 
 puts "There are #{parsed_config.length} differences in reservation data"
 puts "Take a look at ./data/reservation_diff.json for a breakdown"
