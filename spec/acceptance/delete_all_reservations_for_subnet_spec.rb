c213e30636eedb4e6ec4960cc1356d9b2c6a534a
Daniel Wilson <68431297+wanieldilson@users.noreply.github.com>
2021-10-25 09:55:38 +0100

    :children_crossing: Added site name info to config page (#395)
    * :children_crossing: Added site name info to config page

--- spec/acceptance/delete_all_reservations_for_subnet_spec.rb
+++ spec/acceptance/delete_all_reservations_for_subnet_spec.rb
@@ -2,19 +2,26 @@ require "rails_helper"
 
 describe "delete all reservations in a subnet", type: :feature do
   let(:subnet) { create :subnet }
+  let(:subnet2) { create :subnet }
   let(:editor) { create(:user, :editor) }
 
   before do
     login_as editor
   end
 
-  it "delete all reservations" do
-    reservation1 = create :reservation, subnet: subnet
-    reservation2 = create(:reservation,
+  it "deletes all reservations in the given subnet, without affecting any others" do
+    first_reservation_in_subnet = create :reservation, subnet: subnet
+    second_reservation_in_subnet = create :reservation,
       subnet: subnet,
       hw_address: "ab:bb:cc:dd:ee:ff",
       ip_address: subnet.end_address,
-      hostname: "test2.example.com")
+      hostname: "test2.example.com"
+
+    reservation_in_another_subnet = create :reservation,
+      subnet: subnet2,
+      hw_address: "a1:b5:c8:d4:e1:ff",
+      ip_address: subnet2.start_address,
+      hostname: "test99.example.com"
 
     visit "/subnets/#{subnet.to_param}"
 
@@ -22,6 +29,7 @@ describe "delete all reservations in a subnet", type: :feature do
 
     expect(page).to have_content("Are you sure you want to delete all reservations for the following subnet?")
     expect(page).to have_content(subnet.cidr_block)
+    expect(page).to have_content(subnet.shared_network.site.name)
 
     expect_config_to_be_verified
     expect_config_to_be_published
@@ -30,10 +38,14 @@ describe "delete all reservations in a subnet", type: :feature do
 
     expect(page).to have_content("Successfully deleted all reservations for subnet #{subnet.cidr_block}")
     expect(page).to have_content("This could take up to 10 minutes to apply.")
-    expect(page).not_to have_content(reservation1.hw_address)
-    expect(page).not_to have_content(reservation2.hw_address)
+    expect(page).not_to have_content(first_reservation_in_subnet.hw_address)
+    expect(page).not_to have_content(second_reservation_in_subnet.hw_address)
 
     expect_audit_log_entry_for(editor.email, "delete all reservations", "Subnet (#{subnet.cidr_block})")
+
+    visit "/subnets/#{subnet2.to_param}"
+
+    expect(page).to have_content(reservation_in_another_subnet.hw_address)
   end
 
   it "there are no reservations and as such deletion is not an option to the user" do
