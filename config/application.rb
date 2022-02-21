922106c889371b31179d20bdd65f8035c29e924a
astrobinson <aaron.robinson@digital.justice.gov.uk>
2022-02-21 14:31:02 +0000

    set legacy connection handling to false

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module StaffDeviceDnsDhcpAdmin
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.active_record.legacy_connection_handling = false
     # Force HTTPS for all requests except healthcheck endpoint
     config.force_ssl = true
     config.ssl_options = {
