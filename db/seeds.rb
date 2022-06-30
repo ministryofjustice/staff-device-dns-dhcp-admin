f0739c2f8c402de92f71ff88e9f37e59f75656dc
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-06-30 12:30:11 +0100

    :wrench: Modify exclusion start and end address / tidy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,4 @@
-require "ipaddr"
 require "faker"
-
-def random_mac
-  6.times.map { "%02x" % rand(0..255) }.join(":")
-end
-
-# def end_range(range)
-#   range_array = range.to_s.split(".")
-#   last_char = range_array.last.to_i + 24
-#   range_array[0].to_s + "." + range_array[1].to_s + "." + range_array[2].to_s + "." + last_char.to_s
-# end
-
-# def ip_range(subnet_count, count)
-#   "#{count + 10}.0.0.#{subnet_count}"
-# end
 
 def range(site_index, subnet_index)
   "#{site_index + 10}.0.#{subnet_index + 1}.0"
@@ -28,6 +13,18 @@ end
 def end_address(range)
   range_array = range.to_s.split(".")
   last_char = range_array.last.to_i + 254
+  range_array[0].to_s + "." + range_array[1].to_s + "." + range_array[2].to_s + "." + last_char.to_s
+end
+
+def exclusion_start_address(range)
+  range_array = range.to_s.split(".")
+  last_char = range_array.last.to_i + rand(30..40)
+  range_array[0].to_s + "." + range_array[1].to_s + "." + range_array[2].to_s + "." + last_char.to_s
+end
+
+def exclusion_end_address(range)
+  range_array = range.to_s.split(".")
+  last_char = range_array.last.to_i + rand(80..90)
   range_array[0].to_s + "." + range_array[1].to_s + "." + range_array[2].to_s + "." + last_char.to_s
 end
 
@@ -47,7 +44,7 @@ def main
       rand(5..20).times.with_index(0) do |random_number, index|
         subnet.reservations.create!(ip_address: reserved_address(range, index).to_s, hostname: "#{Faker::Internet.domain_word}-#{index}", hw_address: Faker::Internet.mac_address)
       end
-      subnet.exclusions.create!(start_address: start_address(range), end_address: end_address(range))
+      subnet.exclusions.create!(start_address: exclusion_start_address(range), end_address: exclusion_end_address(range))
       p end_address(range)
     end
   end
