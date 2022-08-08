a244d3184994c8d8cd1e9642e68b5007000ff4ac
Gary <26419401+Gary-H9@users.noreply.github.com>
2022-08-08 11:43:24 +0100

    🚒 Add custom 404 + 500 error handling (#559)
    * :truck: Add custom error handling (pages and routing)
    
    Co-authored-by: Yusuf <47318342+yusufsheiqh@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -48,5 +48,10 @@ Rails.application.routes.draw do
   get "/api/dhcp-stats", to: "api/dhcp_stats#index"
   post "/import", to: "import#create"
 
+  get "errors/not_found"
+  match "/404", via: :all, to: "errors#not_found"
+  get "errors/server_error"
+  match "/500", via: :all, to: "errors#server_error"
+
   root "home#index"
 end
