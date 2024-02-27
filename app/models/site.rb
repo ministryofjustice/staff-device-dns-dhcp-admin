bf8f25f09cb1bb8486849b43ac81884dc11caa8d
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2024-02-27 17:33:55 +0000

    Search by Hostname included in MAC Search
    The feature of searching by hostname has been added to the serach by MAC
    address.

--- app/models/site.rb
+++ app/models/site.rb
@@ -22,7 +22,7 @@ class Site < ApplicationRecord
 
   scope :with_mac_search, ->(query) {
       joins("INNER JOIN shared_networks ON shared_networks.site_id = sites.id INNER JOIN subnets ON subnets.shared_network_id = shared_networks.id INNER JOIN reservations ON reservations.subnet_id = subnets.id")
-        .where('reservations.hw_address LIKE ? ' ,"%#{query}%")
+        .where('reservations.hw_address LIKE ? or reservations.hostname LIKE ?' ,"%#{query}%", "%#{query}%")
         .select('sites.fits_id, sites.name, subnets.cidr_block, reservations.hw_address, reservations.ip_address, reservations.hostname, reservations.id')
 
   }
