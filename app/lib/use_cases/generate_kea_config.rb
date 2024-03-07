0c7fa06d82a7aeefc2e3496170bf211bc8e47976
Dare Olufunmilayo <dare.oluumil@digital.justice.gov.uk>
2024-03-07 14:28:26 +0000

    Update kea config to DEBUG (#765)

--- app/lib/use_cases/generate_kea_config.rb
+++ app/lib/use_cases/generate_kea_config.rb
@@ -261,8 +261,8 @@ module UseCases
                   output: "stdout"
                 }
               ],
-              severity: "WARN",
-              debuglevel: 0
+              severity: "DEBUG",
+              debuglevel: 99
             }
           ],
           "hooks-libraries": [
