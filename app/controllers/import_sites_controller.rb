bf8ab02bab330e7de1198c24847f9d34e593692d
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-06-13 21:55:30 +0100

    Add error handling to import controllers
    and remove update_dhcp_config from import operation.
    
    ND-43

--- app/controllers/import_sites_controller.rb
+++ app/controllers/import_sites_controller.rb
@@ -1,24 +1,42 @@
-  class ImportSitesController < ApplicationController
-  def index
-    authorize! :manage, :import_sites
-  end
+class ImportSitesController < ApplicationController
+  before_action :authorize_import_sites
 
-  def new
-    authorize! :manage, :import_sites
-  end
+  def index; end
+
+  def new; end
 
   def create
-    authorize! :manage, :import_sites
-    @result = update_dhcp_config.call(nil, -> { csv_import_sites })
-    if @result.success?
+    if csv_import_sites
       redirect_to import_sites_path, notice: "Successfully ran the sites import."
     else
+      flash[:alert] = "Failed to import sites."
       render :new
     end
+  rescue StandardError => e
+    flash[:alert] = "An error occurred: #{e.message}"
+    render :new
   end
 
+  def run_update_dhcp_config
+    @site = Site.first
+    dhcp_result = update_dhcp_config.call(@site, -> { @site.save! })
+
+    if dhcp_result && dhcp_result.success?
+      redirect_to import_sites_path, notice: "Successfully updated the DHCP (KEA) Config."
+    else
+      flash[:alert] = "Failed to update the DHCP (KEA) Config." unless import_result&.success?
+      render :index
+    end
+  rescue StandardError => e
+    flash[:alert] = "An error occurred: #{e.message}"
+    render :index
+  end
 
   private
+
+  def authorize_import_sites
+    authorize! :manage, :import_sites
+  end
 
   def import_params
     params.require(:import).permit(:file)
@@ -26,15 +44,35 @@
 
   def csv_import_sites
     require 'csv'
-    file = File.open(import_params[:file])
-    csv = CSV.parse(file, headers: true, col_sep: ',')
-    csv.each do |row|
-      site_hash = {}
-      site_hash[:name] = row['SITENAME']
-      site_hash[:fits_id] = row['FITSID']
-      site_hash[:windows_update_delivery_optimisation_enabled] = row['windows_opt']
-      Site.find_or_create_by!(site_hash)
+    file = import_params[:file]
+
+    if file.nil?
+      raise "No file uploaded"
     end
+
+    CSV.open(file.path, headers: true, col_sep: ',') do |csv|
+      ActiveRecord::Base.transaction do
+        csv.each do |row|
+          create_or_update_site(row)
+        end
+      end
+    end
+    true
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound => e
+    raise "Database error: #{e.message}"
+  rescue CSV::MalformedCSVError => e
+    raise "CSV parsing error: #{e.message}"
+  rescue StandardError => e
+    raise "An unexpected error occurred: #{e.message}"
   end
 
+  def create_or_update_site(row)
+    site_attributes = {
+      name: row['SITENAME'],
+      fits_id: row['FITSID'],
+      windows_update_delivery_optimisation_enabled: row['windows_opt']
+    }
+
+    Site.find_or_create_by!(site_attributes)
+  end
 end
--- app/controllers/import_sites_subnets_controller.rb
+++ app/controllers/import_sites_subnets_controller.rb
@@ -1,19 +1,25 @@
-  class ImportSitesSubnetsController < ApplicationController
-  def index
-    authorize! :manage, :import_sites
-  end
+class ImportSitesSubnetsController < ApplicationController
+  before_action :authorize_import_sites
+
+  def index; end
 
   def create
-    authorize! :manage, :import_sites
-    @result = update_dhcp_config.call(nil, -> { csv_import_subnets })
-    if @result.success?
+    if csv_import_subnets
       redirect_to import_sites_path, notice: "Successfully ran the Subnets import."
     else
+      flash[:alert] = "Failed to import subnets."
       render :index
     end
+  rescue StandardError => e
+    flash[:alert] = "An error occurred: #{e.message}"
+    render :index
   end
 
   private
+
+  def authorize_import_sites
+    authorize! :manage, :import_sites
+  end
 
   def import_params
     params.require(:import).permit(:file)
@@ -21,23 +27,43 @@
 
   def csv_import_subnets
     require 'csv'
-    file = File.open(import_params[:file])
-    csv = CSV.parse(file, headers: true, col_sep: ',')
-    csv.each do |row|
-      @site = Site.where(fits_id: row['FITSID']).first!
+    file = import_params[:file]
 
-      @shared_network = SharedNetwork.new(site_id: @site.id)
-      @shared_network.save!
-
-      subnet_hash = {}
-      subnet_hash[:cidr_block] = row['cidr_block']
-      subnet_hash[:start_address] = row['start_address']
-      subnet_hash[:end_address] = row['end_address']
-      subnet_hash[:routers] = row['routers']
-      subnet_hash[:shared_network] = @shared_network
-
-      @subnet = Subnet.find_or_create_by!(subnet_hash)
+    if file.nil?
+      raise "No file uploaded"
     end
+
+    CSV.open(file.path, headers: true, col_sep: ',') do |csv|
+      ActiveRecord::Base.transaction do
+        csv.each do |row|
+          site = Site.find_by!(fits_id: row['FITSID'])
+          shared_network = create_shared_network(site)
+          create_or_update_subnet(row, shared_network)
+        end
+      end
+    end
+    true
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound => e
+    raise "Database error: #{e.message}"
+  rescue CSV::MalformedCSVError => e
+    raise "CSV parsing error: #{e.message}"
+  rescue StandardError => e
+    raise "An unexpected error occurred: #{e.message}"
   end
 
+  def create_shared_network(site)
+    SharedNetwork.create!(site_id: site.id)
+  end
+
+  def create_or_update_subnet(row, shared_network)
+    subnet_attributes = {
+      cidr_block: row['cidr_block'],
+      start_address: row['start_address'],
+      end_address: row['end_address'],
+      routers: row['routers'],
+      shared_network: shared_network
+    }
+
+    Subnet.find_or_create_by!(subnet_attributes)
+  end
 end
